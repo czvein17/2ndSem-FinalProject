5106b56a30bc3172e5ee9d40082c2c5dbbb9cfc3
Czvein Unarce <czveinlei17@gmail.com>
2024-12-30 23:40:43 +0800

    Added vercel.json for FE

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,27 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
+const url = require("url");
 const fs = require("fs");
 
+let mainWindow;
+let loadingWindow;
+
 function createWindow() {
+  // Create the loading window
+  loadingWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  loadingWindow.loadFile(path.join(__dirname, "loading.html"));
+
   const mainWindow = new BrowserWindow({
     title: "Final Project",
     autoHideMenuBar: true,
@@ -17,8 +36,17 @@ function createWindow() {
   });
 
   mainWindow.webContents.openDevTools();
-  mainWindow.loadURL("http://localhost:5173/");
+  // mainWindow.loadURL("http://localhost:5173/");
+  mainWindow.loadFile(path.join(__dirname, "../electron/dist/index.html"));
+
   mainWindow.setMenu(null);
+
+  mainWindow.webContents.on("did-finish-load", () => {
+    // Close the loading window and show the main window
+    if (loadingWindow) loadingWindow.close();
+    mainWindow.show();
+    mainWindow.webContents.openDevTools();
+  });
 
   app.commandLine.appendSwitch("enable-gpu-rasterization");
   app.commandLine.appendSwitch("enable-zero-copy");
