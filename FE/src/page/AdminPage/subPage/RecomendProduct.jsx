bb4244f6bbf0d86f12c17a9a70b59a425f46673a
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-01-07 15:18:20 +0800

    Fixing recommendation ui

--- FE/src/page/AdminPage/subPage/RecomendProduct.jsx
+++ FE/src/page/AdminPage/subPage/RecomendProduct.jsx
@@ -1,19 +1,56 @@
 import React, { useState } from 'react'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
-
-import {
-  MdOutlineEmojiEmotions,
-  MdOutlineSentimentSatisfied,
-  MdOutlineSentimentDissatisfied,
-  MdOutlineSentimentVerySatisfied,
-  MdOutlineSentimentVeryDissatisfied,
-} from 'react-icons/md'
-import { recommendProduct } from '../../../API/product'
 import { useMutation } from '@tanstack/react-query'
+
+import { recommendProduct } from '../../../API/product'
+
+import ANGRY from '../../../assets/emoji/angry.svg'
+import HAPPY from '../../../assets/emoji/happy.svg'
+import ANXIOUS from '../../../assets/emoji/anxious.svg'
+import CHILL from '../../../assets/emoji/chill.svg'
+import SLEEPY from '../../../assets/emoji/sleepy.svg'
+import NEUTRAL from '../../../assets/emoji/neutral.svg'
+import CONFUSED from '../../../assets/emoji/confused.svg'
+import SAD from '../../../assets/emoji/sad.svg'
+
+const ICONS = [
+  {
+    name: 'Neutral',
+    src: NEUTRAL,
+  },
+  {
+    name: 'Chill',
+    src: CHILL,
+  },
+  {
+    name: 'Happy',
+    src: HAPPY,
+  },
+  {
+    name: 'Sleepy',
+    src: SLEEPY,
+  },
+  {
+    name: 'Anxious',
+    src: ANXIOUS,
+  },
+  {
+    name: 'Angry',
+    src: ANGRY,
+  },
+  {
+    name: 'Confused',
+    src: CONFUSED,
+  },
+  {
+    name: 'Sad',
+    src: SAD,
+  },
+]
+
 export const RecomendProduct = () => {
   const [recomendedProducts, setRecomendedProducts] = useState([])
   const [selectedEmotion, setSelectedEmotion] = useState([])
-  const loading = true
 
   console.log(recomendedProducts)
 
@@ -33,7 +70,7 @@ export const RecomendProduct = () => {
   const handleRecommendation = () => recommend(selectedEmotion)
 
   return (
-    <div className='flex h-full md:py-5'>
+    <div className='flex h-full overflow-hidden md:py-5'>
       {isPending && (
         <DotLottieReact
           src='https://lottie.host/c547c13b-5989-4389-8250-1e614f0d7442/tL1laAh5sf.lottie'
@@ -42,33 +79,23 @@ export const RecomendProduct = () => {
         />
       )}
       {!isPending && !recomendedProducts.length && (
-        <div className='flex flex-col mx-auto my-auto space-y-4'>
+        <div className='flex flex-col mx-auto my-auto space-y-10'>
           <div className='mx-auto'>
-            <h4 className='text-2xl font-semibold text-center uppercase'>
+            <h4 className='text-4xl font-semibold text-center uppercase'>
               Hey there, how are you feeling today?
             </h4>
           </div>
-          <div className='flex flex-wrap justify-center gap-1'>
-            <MdOutlineEmojiEmotions
-              className='w-28 h-28 md:w-[200px] md:h-[200px]'
-              onClick={() => handleEmotionClick('Very Happy')}
-            />
-            <MdOutlineSentimentSatisfied
-              className='w-28 h-28 md:w-[200px] md:h-[200px]'
-              onClick={() => handleEmotionClick('Happy')}
-            />
-            <MdOutlineSentimentDissatisfied
-              className='w-28 h-28 md:w-[200px] md:h-[200px]'
-              onClick={() => handleEmotionClick('Sad')}
-            />
-            <MdOutlineSentimentVerySatisfied
-              className='w-28 h-28 md:w-[200px] md:h-[200px]'
-              onClick={() => handleEmotionClick('Very Satisfied')}
-            />
-            <MdOutlineSentimentVeryDissatisfied
-              className='w-28 h-28 md:w-[200px] md:h-[200px]'
-              onClick={() => handleEmotionClick('Very Dissatisfied')}
-            />
+
+          <div className='flex flex-wrap justify-center gap-10'>
+            {ICONS.map((icon, index) => (
+              <img
+                key={index}
+                src={icon.src}
+                alt={icon.name}
+                className='w-24 h-24 md:w-[200px] md:h-[200px] drop-shadow-2xl '
+                onClick={() => handleEmotionClick(icon.name)}
+              />
+            ))}
           </div>
           <button
             className='px-5 py-2 mx-auto rounded bg-primary'
@@ -80,12 +107,20 @@ export const RecomendProduct = () => {
       )}
 
       {!isPending && recomendedProducts.length > 0 && (
-        <div className='flex flex-col gap-2'>
-          {recomendedProducts.map((product) => (
-            <div key={product._id} className='p-5 border border-black rounded-lg'>
+        <div className='grid grid-cols-1 gap-5 py-5 overflow-auto bg-red-500 md:py-0 xl:grid-cols-3 md:grid-cols-2 auto-rows-min'>
+          {recomendedProducts.map((product, index) => (
+            <div key={product._id} className={`p-5 border border-black rounded-lg`}>
+              <div className='w-32 h-32 mx-auto'>
+                <img
+                  src={`${import.meta.env.VITE_API_BASE_URL.replace('/api/v1', '')}/images/coffee-image/${product.image}`}
+                  alt={product.name}
+                />
+              </div>
+              <p>USER MOOD</p>
               <p>{product.name}</p>
               <p>{product.description}</p>
               <p>{product.explanation}</p>
+              <p>APPLICABLE MOOD</p>
             </div>
           ))}
         </div>
