8a106baa8e297d11708a36ffbc79f3df8d5cd07b
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-03 23:34:59 +0800

    Add Sales schema with fields for order, subtotal, tax, discounts, payment mode, and status

--- BE/src/models/Sales.js
+++ BE/src/models/Sales.js
@@ -1,5 +1,55 @@
 const mongoose = require("mongoose");
 
-const SalesSchema = new mongoose.Schema({
-    
-})
+const SalesSchema = new mongoose.Schema(
+  {
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    tax: {
+      type: Number,
+      required: true,
+    },
+    discountAmount: {
+      type: Number,
+      default: 0,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    discountType: {
+      type: String,
+      enum: ["pwd", "senior", "promo", "none"],
+      default: "none",
+    },
+    modeOfPayment: {
+      type: String,
+      enum: ["cash", "card", "paymaya", "gcash"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["paid", "pending", "refunded"],
+      required: true,
+      default: "pending",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Sales = mongoose.model("Sales", SalesSchema);
+module.exports = Sales;
