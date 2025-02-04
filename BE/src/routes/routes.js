a975cc355b1d26a2c16600f3a30a07fae0006788
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-04 19:52:26 +0800

    Add sales routes to main router

--- BE/src/routes/routes.js
+++ BE/src/routes/routes.js
@@ -9,6 +9,7 @@ const productRoutes = require("./productRoutes");
 const ingredientRoutes = require("./ingredientRoutes");
 const reviewRoutes = require("./reviewRoutes");
 const orderRoutes = require("./orderRoutes");
+const salesRoutes = require("./salesRoutes");
 const router = express.Router();
 
 router.use("/signin", signinRoutes);
@@ -19,5 +20,6 @@ router.use("/products", productRoutes);
 router.use("/ingredients", ingredientRoutes);
 router.use("/reviews", reviewRoutes);
 router.use("/orders", orderRoutes);
+router.use("/sales", salesRoutes);
 
 module.exports = router;
