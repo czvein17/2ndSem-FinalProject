d9a9ed99c41de58162f8eae75092223270517e5a
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-04 21:56:55 +0800

    Refactor createOrder function: simplify payload structure and add logging for debugging

--- FE/src/API/order.js
+++ FE/src/API/order.js
@@ -1,9 +1,7 @@
 import http from './http'
 
-export const createOrder = async (cartItems) => {
-  const payload = {
-    orderItems: cartItems,
-  }
+export const createOrder = async (payload) => {
+  console.log('Payload ', payload)
 
   const order = await http.post('/orders', payload)
   return order.data
