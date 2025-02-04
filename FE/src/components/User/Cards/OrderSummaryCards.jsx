938f04bf9b5784c00093bd918649675fbf6e6897
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-04 21:57:26 +0800

    Add OrderSummaryCards component: display order summary details with conditional formatting

---
+++ FE/src/components/User/Cards/OrderSummaryCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const OrderSummaryCards = ({ orderSummary }) => {
+  return (
+    <div className='flex justify-between  border-b-2 border-[#D9D9D9] py-2 px-2'>
+      <h2 className='font-medium'>{orderSummary.title} :</h2>
+      <h2 className='font-medium uppercase text-orange'>
+        {orderSummary.title !== 'Discount Type' && <span>&#8369;</span>}
+        {orderSummary.value}
+      </h2>
+    </div>
+  )
+}
