0590477859c8f97abc004c4e0321dcaf05d5f2e4
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-04 19:52:23 +0800

    Enhance order routes: add getAllOrders and updateOrder functionalities

--- BE/src/routes/orderRoutes.js
+++ BE/src/routes/orderRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getOrderById } = require("../controllers/orderController");
+const {
+  getAllOrders,
+  createOrder,
+  getOrderById,
+  updateOrder,
+} = require("../controllers/orderController");
 
 const { protectAuth } = require("../middlewares/protectAuth");
 
 router.use(protectAuth);
-router.post("/", createOrder);
-router.route("/:id").get(getOrderById);
+
+router.route("/").get(getAllOrders).post(createOrder);
+router.route("/:id").get(getOrderById).patch(updateOrder);
 
 module.exports = router;
