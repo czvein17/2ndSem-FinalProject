e44e9d9cc5e8bbbede8d4c7b659534584004d420
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-20 02:47:08 +0800

    Replace Sidebar component with UserSidebar in UserLayout for improved structure

--- FE/src/layout/User/UserLayout.jsx
+++ FE/src/layout/User/UserLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Sidebar } from '../../components/User/Sidebar'
+import { UserSidebar } from '../../components/Sidebar/UserSidebar'
 import { Outlet } from 'react-router-dom'
 
 export const UserLayout = () => {
   return (
     <div className='flex h-screen overflow-hidden bg-secondBg font-poppins'>
-      <Sidebar />
+      <UserSidebar />
       <div className='w-full min-h-screen overflow-auto'>
         <Outlet />
       </div>
