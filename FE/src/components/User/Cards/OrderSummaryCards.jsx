c724eb71985de5938042d6cc64bc159e2d8f9e87
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-20 02:46:42 +0800

    Adjust padding in OrderSummaryCards for improved layout consistency

--- FE/src/components/User/Cards/OrderSummaryCards.jsx
+++ FE/src/components/User/Cards/OrderSummaryCards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const OrderSummaryCards = ({ orderSummary }) => {
   return (
-    <div className='flex justify-between  border-b-2 border-[#D9D9D9] py-2 px-2'>
+    <div className='flex justify-between  border-b-2 border-[#D9D9D9] py-1 px-2'>
       <h2 className='font-medium'>{orderSummary.title} :</h2>
       <h2 className='font-medium uppercase text-orange'>
         {orderSummary.title !== 'Discount Type' && <span>&#8369;</span>}
