5abbcbe851b1347b9584ca14c3aa1cc08cae44ec
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-02-20 02:47:19 +0800

    Add Inventory Dashboard component with basic layout

---
+++ FE/src/page/Inventory/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Dashboard = () => {
+  return (
+    <div className='h-full overflow-hidden bg-red-300'>
+      <div className='h-full p-5 space-y-3 overflow-auto'>
+        <div className='h-[400px] bg-black'>1</div>
+        <div className='h-[400px] bg-black'>1</div>
+        <div className='h-[400px] bg-black'>1</div>
+      </div>
+    </div>
+  )
+}
