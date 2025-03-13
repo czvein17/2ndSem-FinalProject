027618f09b58c59223a18262a3ac274d7c5001ca
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-03-13 21:42:12 +0800

    Add cash payment processing function to handle payments with order ID and received amount

--- FE/src/API/payment.js
+++ FE/src/API/payment.js
@@ -1,5 +1,12 @@
 import http from './http'
 
+export const payWithCash = async ({ orderId, receiveAmount }) => {
+  console.log('orderId', orderId)
+  console.log('receiveAmount', receiveAmount)
+  const response = await http.post(`/payment/cash/${orderId}`, { receiveAmount })
+  return response.data
+}
+
 export const payWithPayMaya = async (order) => {
   const orderId = order.d._id
   const response = await http.post(`/payment/maya/${orderId}`)
