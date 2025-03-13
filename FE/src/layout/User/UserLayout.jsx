cd2f3c5621379719ede81dc4576cd17471659fbf
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-03-13 21:42:57 +0800

    Add ToastContainer to UserLayout for displaying notifications

--- FE/src/layout/User/UserLayout.jsx
+++ FE/src/layout/User/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { UserSidebar } from '../../components/Sidebar/UserSidebar'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 export const UserLayout = () => {
   return (
@@ -9,6 +10,20 @@ export const UserLayout = () => {
       <div className='w-full min-h-screen overflow-auto'>
         <Outlet />
       </div>
+
+      <ToastContainer
+        className='custom-toast-container'
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        closeButton={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   )
 }
