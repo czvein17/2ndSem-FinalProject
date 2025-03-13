3afde9ed9caf4576c753618c78d3576e2c49dadd
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-03-13 21:42:02 +0800

    Add query parameter support for getAllOrders and implement updateOrderStatus and deleteOrder functions

--- FE/src/API/order.js
+++ FE/src/API/order.js
@@ -7,12 +7,25 @@ export const createOrder = async (payload) => {
   return order.data
 }
 
-export const getAllOrders = async () => {
-  const orders = await http.get('orders')
+export const getAllOrders = async (queryParams = {}) => {
+  const orders = await http.get('orders', { params: queryParams })
   return orders.data
 }
 
 export const getOrder = async (orderId) => {
+  console.log('Order Id ', orderId)
   const order = await http.get(`/orders/${orderId}`)
+  return order.data
+}
+
+export const updateOrderStatus = async ({ orderId, status }) => {
+  console.log('Order Id ', orderId)
+  console.log('Status ', status)
+  const order = await http.patch(`/orders/${orderId}`, { status })
+  return order.data
+}
+
+export const deleteOrder = async (orderId) => {
+  const order = await http.delete(`/orders/${orderId}`)
   return order.data
 }
