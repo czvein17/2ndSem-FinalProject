6f3d73565b6cfb294a4be15b06e94afa86658ff0
Czvein Lei Unarce <czveinlei17@gmail.com>
2025-03-13 21:41:23 +0800

    Add cash payment route and debug endpoint to paymentRoutes

--- BE/src/routes/paymentRoutes.js
+++ BE/src/routes/paymentRoutes.js
@@ -3,11 +3,18 @@ const express = require("express");
 const {
   payWithPayMaya,
   confirmPayment,
+  payWithCash,
+  debugMaya,
 } = require("../controllers/PaymentController");
+const { protectAuth } = require("../middlewares/protectAuth");
 
 const router = express.Router();
 
+router.use(protectAuth);
+router.post("/cash/:orderId", payWithCash);
 router.post("/maya/:orderId", payWithPayMaya);
 router.get("/maya-confirm/:orderId", confirmPayment);
+
+router.get("/debug-maya/:checkoutId", debugMaya);
 
 module.exports = router;
